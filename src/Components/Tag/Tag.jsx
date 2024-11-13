6d594f6cd2a3f5d784797b04aae28165394bd41d
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-13 18:03:10 +0100

    add color on tags

--- src/Components/Tag/Tag.jsx
+++ src/Components/Tag/Tag.jsx
@@ -1,11 +1,16 @@
 import style from "./Tag.module.css"
 export default function Tag({ tag }) {
+    let color
+    tag === 'html' ? color = style.html : tag === 'css' ? color = style.css : tag === 'php' ? color = style.php : tag === 'js' ? color = style.js : color = style.badge
+
+
+
 
 
 
     return (
 
-        <span className={style.badge} >{tag}</span>
+        <span className={color}>{tag}</span>
 
 
 
